68537f1e42c50621039073a5c9fd96376ad44fd5
dj-dj <1262284460@qq.com>
2015-08-30 19:25:13 +0800

    Update ViewController.m

--- gittest2/gittest2/ViewController.m
+++ gittest2/gittest2/ViewController.m
@@ -23,6 +23,19 @@
      NSLog(@"dfdsa");
     NSLog(@"derwrwqds");
      NSLog(@"654864s");
+     NSLog(@"dfdsa");
+    NSLog(@"de4156415ds");
+     NSLog(@"65484848");
+     
+     
+     SLog(@"dfdsa");
+    NSLog(@"der452wqds");
+     NSLog(@"6545464s");
+     NSLog(@"545sa");
+    NSLog(@"de4156415ds");
+     NSLog(@"65484848");
+     
+     
 }
 
 - (void)didReceiveMemoryWarning {
